bf3f384a332855b8a6773e5ddcb7c8a0e8d41c9a
João Cruz <jcruz@posteo.net>
2023-06-08 14:46:33 +0100

    Complete generic implementation, with standalone functions

--- projection.cpp
+++ projection.cpp
@@ -1,7 +1,10 @@
 #include <algorithm>
 #include <vector>
 #include <iostream>
+#include <functional>
 #include <ostream>
+#include <cmath>
+#include <utility>
 
 
 
@@ -14,21 +17,25 @@ public:
     Test(double a, int b) : a_{a}, b_{b} { }
     double getA() const { return a_; }
     int getB() const { return b_; }
-    int possibleSelect() const { return a_ + b_; }
+    int possibleSelect() { return a_ + b_; }
 };
 std::ostream& operator<<(std::ostream& o, const Test& t)
 {
     return o << "Test(" << t.getA() << ", " << t.getB() << ")";
 }
 
+double distance(const Test& t1, const Test& t2) {
+    return std::hypot(t2.getA() - t1.getA(), t2.getB() - t1.getB());
+}
 
 
-template <auto Projection, typename Comparator = std::less<>>
-auto projection()
+template <auto Projection, typename Comparator = std::less<>, typename ... ProjArgs>
+auto projection(ProjArgs&& ... args)
 {
-    return [](const auto& x1, const auto& x2) {
-        return Comparator{}((x1.*Projection)(),
-                            (x2.*Projection)());
+    return [...args = std::forward<ProjArgs>(args)](const auto& x1, const auto& x2) {
+        return Comparator{}(
+            std::invoke(Projection, x1, args...),
+            std::invoke(Projection, x2, args...));
     };
 }
 
@@ -38,16 +45,21 @@ int main()
     std::vector<Test> v{
         {12.2,2}, {20.4,43}, {3.1,64}, {210.32,6}, {0.11,3}
     };
+    Test tx{1.0,1};
 
-    std::sort(v.begin(), v.end(), projection<&Test::getA, std::greater<>>());
+    std::sort(v.begin(), v.end(), projection<&Test::getA>());
+    for (const Test& t : v) { std::cout << t << " " << distance(tx, t) << "\n"; }
+    std::cout << "\n";
 
-    for (const Test& t : v) { std::cout << t << "\n"; }
+    auto m1 = std::max_element(v.cbegin(), v.cend(), projection<&Test::getB>());
+    std::cout << "max getB: " << *m1 << "\n";
+    std::cout << "\n";
 
-    std::cout << "---\n";
+    auto m2 = std::min_element(v.cbegin(), v.cend(), projection<distance>(tx));
+    std::cout << "min distance to tx: " << *m2 << "\n";
+    std::cout << "\n";
 
-    std::sort(v.begin(), v.end(), [](const Test& t1, const Test& t2) {
-        return t1.getB() < t2.getB();
-    });
-
-    for (const Test& t : v) { std::cout << t << "\n"; }
+    std::sort(v.begin(), v.end(), projection<distance>(tx));
+    for (const Test& t : v) { std::cout << t << " " << distance(tx, t) << "\n"; }
+    std::cout << "\n";
 }
